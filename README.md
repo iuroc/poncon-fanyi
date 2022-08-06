80015ab4fb36b580a8b101de3a6885f161b396d3
oyps <ponconsoft@gmail.com>
2022-08-06 21:31:08 +0800

    初始化

--- README.md
+++ README.md
@@ -1,10 +1,24 @@
-# 鹏创翻译
+# poncon
 
-> 调用有道翻译API实现的在线翻译网站
+## Project setup
+```
+npm install
+```
 
-## 项目信息
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
 
-- 作者：欧阳鹏（鹏优创工作室）
-- 开发时间：2022年8月6日（祖国我爱你）
-- 开发语言：HTML、CSS、JavaScript、PHP
-- 使用框架：jQuery、Bootstrap
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,87 @@
+<template>
+    <nav class="navbar navbar-expand navbar-light bg-light border-bottom shadow-sm">
+        <div class="container">
+            <a class="navbar-brand ms-2" href="">鹏创翻译</a>
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" href="https://ouyangpeng.top"
+                        >官网</a
+                    >
+                    <a class="nav-link" href="#">Github</a>
+                    <a class="nav-link" href="#">关于</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <div class="container py-3 py-sm-4">
+        <div class="row">
+            <div
+                class="col-lg-6"
+                v-for="(item, index) in items"
+                v-bind:key="index"
+            >
+                <div class="form-floating mb-3 mb-lg-4">
+                    <textarea
+                        class="form-control"
+                        v-model="item.value"
+                        placeholder="待翻译内容"
+                        style="height: 200px"
+                    ></textarea>
+                    <label>{{ item.text }}</label>
+                </div>
+                <div v-if="!index" class="row mb-3 mb-lg-0 mx-0">
+                    <div class="col-6 pe-2 pe-sm-3 ps-0">
+                        <button class="btn w-100 btn-success text-nowrap me-3">
+                            选择语言
+                        </button>
+                    </div>
+                    <div class="col-3 pe-2 pe-sm-3 ps-0">
+                        <button class="btn w-100 text-nowrap btn-danger">
+                            清空
+                        </button>
+                    </div>
+                    <div class="col-3 px-0">
+                        <button class="btn w-100 text-nowrap btn-primary">
+                            翻译
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App",
+    data() {
+        return {
+            items: [
+                {
+                    text: "待翻译内容",
+                    value: "",
+                },
+                {
+                    text: "翻译结果",
+                    value: "",
+                },
+            ],
+        };
+    },
+    components: {},
+};
+</script>
+
+<style>
+</style>
