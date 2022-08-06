ec54680cc50dba45631c5c5ca740f0a7c0fba96d
oyps <ponconsoft@gmail.com>
2022-08-06 10:22:27 +0800

    搁置

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>鹏创翻译</title>
+    <meta name="description" content="鹏创翻译，基于有道翻译API提供在线翻译服务">
+    <link rel="stylesheet" href="https://cdn.staticfile.org/bootstrap/4.6.0/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/index.css<?php echo '?_=' . time(); ?>">
+    <script src="https://cdn.staticfile.org/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdn.staticfile.org/bootstrap/4.6.0/js/bootstrap.bundle.min.js"></script>
+    <script src="js/index.js<?php echo '?_=' . time(); ?>"></script>
+</head>
+
+<body>
+
+</body>
+
+</html>
